7136c5a5466d70ab8a5fe4cf30b4507f63b1026f
Sudhanva Rajesh <sudhanva0001@gmail.com>
2025-02-07 09:14:30 -0600

    working oauth code

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -313,6 +313,6 @@ Devise.setup do |config|
   config.omniauth :google_oauth2, ENV['GOOGLE_OAUTH_CLIENT_ID'], ENV['GOOGLE_OAUTH_CLIENT_SECRET'], {
     scope: 'email,profile',
     prompt: 'select_account',
-    redirect_uri: 'https://oauth-test-yayay-2dbdc5b25bce.herokuapp.com/admins/auth/google_oauth2/callback'
+    redirect_uri: "http://127.0.0.1:3000/admins/auth/google_oauth2/callback"
   }
 end
