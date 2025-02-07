941022dca1656323d3aed2e326e70b8c40fab5b3
Samraat Gupta <samraatg@tamu.edu>
2025-02-06 22:16:19 -0600

    devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -313,6 +313,6 @@ Devise.setup do |config|
   config.omniauth :google_oauth2, ENV['GOOGLE_OAUTH_CLIENT_ID'], ENV['GOOGLE_OAUTH_CLIENT_SECRET'], {
     scope: 'email,profile',
     prompt: 'select_account',
-    redirect_uri: 'http://localhost:3000/admins/auth/google_oauth2/callback'
+    redirect_uri: 'https://oauth-test-yayay-2dbdc5b25bce.herokuapp.com/admins/auth/google_oauth2/callback'
   }
 end
