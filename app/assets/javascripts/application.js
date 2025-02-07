db8aeccfdf2ae2daeb80f4ecaf59644e217eee75
Samraat Gupta <samraatg@tamu.edu>
2025-02-06 22:00:16 -0600

    init

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,7 @@
+// This is the main JavaScript manifest file for your Rails app.// It will include all other JavaScript files listed below.
+//= require rails-ujs
+
+//= require_tree .
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("JavaScript is loaded and working!");
+});
